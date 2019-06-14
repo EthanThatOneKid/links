942f727b0b9241913a926e7a5d5e0a1614060cc1
Ethan <ethan.r.davidson@gmail.com>
2019-06-14 12:04:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Dependencies
-const {readFileSync: r} = require('fs');
-const {MarkdownBuilder} = require('md-builder'); // https://github.com/yulric/md-builder
+const {readFileSync: r, writeFileSync: w} = require('fs');
+const {MarkdownBuilder: MDB} = require('md-builder'); // https://github.com/yulric/md-builder
 const {recursiveEntries, valueFromPath} = require('recursive-entries');
 
 // Constants
@@ -9,6 +9,37 @@ const links = recursiveEntries(JSON.parse(r('./links.json')));
 // Main Process
 (() => {
 
-  const md = MarkdownBuilder.h1("🔗 EthanThatOneKid's Links");
+  const md = MDB.h1("🔗 EthanThatOneKid's Links");
+
+  let gimmeList = [];
+  for (let [path, value] of links) {
+
+    const depth = ~~(path.length * 0.5 + 1);
+    const endpoint = path[path.length - 1];
+
+    if ("icon".includes(endpoint)) {
+
+      if (!!gimmeList.length) {
+
+        md.unorderedList(gimmeList);
+        gimmeList = [];
+
+      }
+
+      const title = path[path.length - 2];
+      md[`h${depth}`](`${value} ${title}`);
+
+    } else if ("href".includes(endpoint)) {
+
+      const linkTo = value;
+      const linkLabel = path[path.length - 2];
+      gimmeList.push(MDB.link({linkTo, linkLabel}));
+
+    }
+
+  }
+
+  const serializedMarkdown = md.toMarkdown();
+  w("./links.md");
 
 })();
