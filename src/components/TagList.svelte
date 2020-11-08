b4f74d6d17ae8508bc5b9fbb1e39b3d55af98741
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-11-07 22:04:15 -0800

    Create TagList.svelte

---
+++ src/components/TagList.svelte
@@ -0,0 +1,28 @@
+<!-- Reference:
+- https://www.w3.org/TR/2017/WD-wai-aria-practices-1.1-20170628/examples/radio/radio-1/radio-1.html#kbd_label
+-->
+<script lang="ts">
+import { createEventDispatcher } from "svelte";
+import { keyboard } from "../actions/keyboard";
+
+export let value: string[] = [];
+
+const radiogroupHandlers = {};
+</script>
+
+<div role="radiogroup" tabindex="{0}" use:keyboard="{radiogroupHandlers}">
+  {#each value as tag}
+    <div role="radio" aria-checked="{false}">{tag}</div>
+  {/each}
+</div>
+
+<!-- <ul tabindex="{0}">
+  {#each value as tag}
+    <li
+      on:click="{() => handleRemoveTag(tag)}"
+      on:focus="{() => handleFocusTag(tag)}"
+    >
+      {tag}
+    </li>
+  {/each}
+</ul> -->
